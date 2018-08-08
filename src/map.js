74cf1172eeece8a0706b78c78a015a3f49c35d28
Egor Gugaev <egor@gugaevegor.ru>
2018-08-08 19:44:41 +0300

    Решение задачи

--- src/map.js
+++ src/map.js
@@ -2,52 +2,49 @@ import { loadList, loadDetails } from './api';
 import { getDetailsContentLayout } from './details';
 import { createFilterControl } from './filter';
 
-export function initMap(ymaps, containerId) {
-  const myMap = new ymaps.Map(containerId, {
-    center: [55.76, 37.64],
-    controls: [],
-    zoom: 10
-  });
+export default function initMap(ymaps, containerId) {
+    const myMap = new ymaps.Map(containerId, {
+            center: [55.76, 37.64],
+            controls: [],
+            zoom: 10
+        }),
+        objectManager = new ymaps.ObjectManager({
+            clusterize: true,
+            gridSize: 64,
+            clusterIconLayout: 'default#pieChart',
+            clusterDisableClickZoom: false,
+            geoObjectOpenBalloonOnClick: false,
+            geoObjectHideIconOnBalloonOpen: false,
+            geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
+        });
+    myMap.geoObjects.add(objectManager);
+    loadList().then(data => {
+        objectManager.add(data);
+    });
 
-  const objectManager = new ymaps.ObjectManager({
-    clusterize: true,
-    gridSize: 64,
-    clusterIconLayout: 'default#pieChart',
-    clusterDisableClickZoom: false,
-    geoObjectOpenBalloonOnClick: false,
-    geoObjectHideIconOnBalloonOpen: false,
-    geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
-  });
+    // details
+    objectManager.objects.events.add('click', event => {
+        const objectId = event.get('objectId');
+        const obj = objectManager.objects.getById(objectId);
 
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+        objectManager.objects.balloon.open(objectId);
 
-  loadList().then(data => {
-    objectManager.add(data);
-  });
+        if (!obj.properties.details) {
+            loadDetails(objectId).then(data => {
+                obj.properties.details = data;
+                objectManager.objects.balloon.setData(obj);
+            });
+        }
+    });
 
-  // details
-  objectManager.objects.events.add('click', event => {
-    const objectId = event.get('objectId');
-    const obj = objectManager.objects.getById(objectId);
+    // filters
+    const listBoxControl = createFilterControl(ymaps);
+    myMap.controls.add(listBoxControl);
 
-    objectManager.objects.balloon.open(objectId);
-
-    if (!obj.properties.details) {
-      loadDetails(objectId).then(data => {
-        obj.properties.details = data;
-        objectManager.objects.balloon.setData(obj);
-      });
-    }
-  });
-
-  // filters
-  const listBoxControl = createFilterControl(ymaps);
-  myMap.controls.add(listBoxControl);
-
-  var filterMonitor = new ymaps.Monitor(listBoxControl.state);
-  filterMonitor.add('filters', filters => {
-    objectManager.setFilter(
-      obj => filters[obj.isActive ? 'active' : 'defective']
-    );
-  });
+    var filterMonitor = new ymaps.Monitor(listBoxControl.state);
+    filterMonitor.add('filters', filters => {
+        objectManager.setFilter(
+            obj => filters[obj.isActive ? 'active' : 'defective']
+        );
+    });
 }
